5cfab9e0573f9db3aa1d72548b84e30c3eea1522
flookbozo <f_l_o_o_k@hotmail.com>
2022-04-12 12:35:04 +0700

    clear confilct

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
 	fmt.Println("haha")
 	fmt.Println("hehe")
-=======
 	fmt.Println("Hello")
->>>>>>> 6543f8f096af5130e97dfe413c15671d6fa31d4f
 }
